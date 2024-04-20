eea6cd9ddfb85a3fe01b763d558ac1259cd8a6cb
yang <2839309325@qq.com>
2024-04-20 18:43:15 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -18,7 +18,7 @@ fn main() {
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5];
+    let my_empty_vec = vec![1, 2, 3, 4, 5, 6];
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
